050da7bacef3459eddd886628aca326a7356218f
Bhupendra Singh <bhupi29491@gmail.com>
2024-10-16 20:33:55 +0300

    movies-service : Implemented the movies-service to call the movieInfo & movieReview microservice. Also handled Not Found exception at MoviesInfoRestClient.java

---
+++ movies-service/src/main/java/com/reactivespring/controller/MoviesController.java
@@ -0,0 +1,37 @@
+package com.reactivespring.controller;
+
+
+import com.reactivespring.client.MoviesInfoRestClient;
+import com.reactivespring.client.ReviewsRestClient;
+import com.reactivespring.domain.Movie;
+import com.reactivespring.domain.Review;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/movies")
+public class MoviesController {
+
+    private MoviesInfoRestClient moviesInfoRestClient;
+    private ReviewsRestClient reviewsRestClient;
+
+    public MoviesController(MoviesInfoRestClient moviesInfoRestClient, ReviewsRestClient reviewsRestClient) {
+        this.moviesInfoRestClient = moviesInfoRestClient;
+        this.reviewsRestClient = reviewsRestClient;
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Movie> fetchMovieById(@PathVariable("id") String movieId) {
+        return moviesInfoRestClient.fetchMovieInfo(movieId)
+                                   .flatMap(movieInfo -> {
+                                       Mono<List<Review>> reviewsListMono = reviewsRestClient.fetchReviews(movieId)
+                                                                                             .collectList();
+                                       return reviewsListMono.map(reviews -> new Movie(movieInfo, reviews));
+                                   });
+    }
+}
